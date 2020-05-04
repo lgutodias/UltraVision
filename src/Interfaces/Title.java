c0ab687455a4f14929c92fc80568267907ec2725
lgutodias <dias.luizaugusto@gmail.com>
2020-05-04 16:33:06 +0100

    Create interface Title

---
+++ src/Interfaces/Title.java
@@ -0,0 +1,20 @@
+package Interfaces;
+
+public interface Title<T> {
+	
+	public int getId();
+	public T setTitle(int id);
+	
+	public String getTitle();
+	public T setTitle(String title);
+	
+	public String getGenre();
+	public T setGenre(String genre);
+	
+	public int getYearOfRelease();
+	public T setYearOfRelease(int year);
+	
+	public String getMediaFormat();
+	public T setMediaFormat(String media);
+
+}
