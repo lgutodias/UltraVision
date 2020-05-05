4706245f900faec5ada183f4f7478672951796c6
lgutodias <dias.luizaugusto@gmail.com>
2020-05-05 14:00:59 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import Interfaces.Title;
+import Model.Customer;
 
 public class Main {
+	
+	static List<Title> titles = new ArrayList<>();
+	static List<Customer> customers = new ArrayList<>();
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
