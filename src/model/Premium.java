7060ef83eb2572a73e4426bd3ad3228d4176d923
dgutierres26 <diegogutierres@hotmail.com>
2020-05-05 16:03:14 +0100

    Create Premium.java

---
+++ src/model/Premium.java
@@ -0,0 +1,42 @@
+/**
+ * @author diego
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import interfaces.MembershipPlan;
+import interfaces.Title;
+
+public class Premium implements MembershipPlan<Title> {
+	
+	//Attribute
+	private List<Title> titles = new ArrayList<>();
+	
+	@Override
+	public List<Title> getAll(){
+		return this.titles;
+	}
+	@Override
+	public void add(Title m) {
+		this.titles.add(m);
+	}
+	@Override
+	public void delete(int index) {
+		this.titles.remove(this.titles.get(index));
+	}
+	@Override
+	public void delete(Title m) {
+		this.titles.remove(m);
+	}
+	@Override
+	public int size() {
+		return this.titles.size();
+	}
+	@Override
+	public Title get(int index) {
+		return this.titles.get(index);
+	}
+	
+	
+}
